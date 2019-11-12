2e91dfdb4c29eb14caa944fde44f8240600b781c
Angel Reyes <miickeyreyez@gmail.com>
2019-11-12 10:11:09 -0600

    Quicksort

--- 7.sorting/7.quickSort.js
+++ 7.sorting/7.quickSort.js
@@ -16,4 +16,14 @@ function pivot(arr, start = 0, end = arr.length + 1) {
   return swapIdx;
 }
 
-pivot ([4, 8, 2, 1, 5, 7, 6, 3]);
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right);
+    quickSort(arr, left, pivotIndex - 1);
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
+
+// pivot ([4, 8, 2, 1, 5, 7, 6, 3]);
+quickSort([4, 6, 9, 1, 2, 5, 3]);
